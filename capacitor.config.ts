aca3a6200a5e0f9c0567ddc6f70573bc7d705cbd
Nazarii Omelchenko <omelchenko.nazarii@gmail.com>
2023-05-01 10:21:51 +0300

    added android capacitor

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.example.app',
+  appName: 'angular-capacitor-test-project',
+  webDir: 'dist/angular-capacitor-test-project',
+  bundledWebRuntime: false
+};
+
+export default config;
